13117dadef6af377520bb0f020daba1fb73b9c03
dhivyadharshini04t <dhivyadharshini04t@gmail.com>
2023-02-22 09:12:13 +0530

    first commit

---
+++ src/main/java/com/example/demo/Nameday12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nameday12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nameday12Application.class, args);
+	}
+
+}
